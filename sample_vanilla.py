3e1750b43f1e549089277845000dc9498e776b0d
amudide <amudide@gmail.com>
2024-11-25 23:18:54 -0500

    clean

--- sample_vanilla.py
+++ sample_vanilla.py
@@ -61,7 +61,7 @@ def main(args):
     samples = vae.decode(samples / 0.18215).sample
 
     # Save and display images:
-    save_image(samples, f"imgs/vanilla_steps{args.num_sampling_steps}_size{args.image_size}.png", nrow=4, normalize=True, value_range=(-1, 1))
+    save_image(samples, f"imgs/vanilla_steps{args.num_sampling_steps}_size{args.image_size}_seed{args.seed}.png", nrow=4, normalize=True, value_range=(-1, 1))
 
 
 if __name__ == "__main__":
--- sweep.py
+++ sweep.py
@@ -13,16 +13,20 @@ import os
 #]
 
 #commands = [
-#    f"python sample_bad.py --image-size 256 --device 7 --num-sampling-steps {i}" for i in range(1, 26)
+#    f"python sample_bad.py --image-size 256 --device 7 --skip {i}" for i in range(28)
 #]
 
 #commands = [
-#    f"python sample_unconditional.py --image-size 256 --device 7 --num-sampling-steps {i}" for i in range(1, 26)
+#    f"python sample_unconditional.py --image-size 256 --device 7"
 #]
 
 commands = [
-    f"python sample_fg.py --device 7 --cfg-scale {i} --skip {j} --seed 1" for i in [1.5, 4.0] for j in range(28)
+    f"python sample_fg_unconditional.py --image-size 256 --cfg-scale {i} --skip {j}" for i in [1.5, 4.0] for j in range(28)
 ]
+
+#commands = [
+#    f"python sample_fg.py --device 7 --cfg-scale {i} --skip {j} --seed 1" for i in [1.5, 4.0] for j in range(28)
+#]
 
 for c in commands:
     print(c)
@@ -32,5 +36,11 @@ for cmd in commands:
 
 '''
 python sample_fg.py --device 7 --cfg-scale 1.5 --skip 14
+
+torchrun --nnodes=1 --nproc_per_node=4 sample_ddp_fg.py --model DiT-XL/2 --num-fid-samples 50000
+
+
+
 '''
+
 
